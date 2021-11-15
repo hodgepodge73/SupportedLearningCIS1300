8df2908f498435e7cb6d2971b5dcb43f85c5320e
Ethan <ehodge@uoguelph.ca>
2021-11-15 10:25:13 -0500

    Working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SupportedLearningCIS1300
+
+This code was developed by Rashi and Ethan for the purpose of the SLG for the course CIS1300.
--- Week10/structs.c
+++ Week10/structs.c
@@ -2,36 +2,36 @@
 //there are 2 problems in this code
 int main(int argc, char const *argv[])
 {
-	FILE* fp = fopen("data.txt", "r");
+	FILE* fp = fopen("attendees1.txt", "r");
 	//
 	participants slg[20];
 	int i;//
 
 	
-	while(!feof){
-		fscanf("%s %d %d ", slg[i].name, slg[i].hours, slg[i].sessions);
+	while(!feof(fp)){
+		fscanf(fp, "%s %d %d ", &slg[i].name, &slg[i].hours, &slg[i].sessions);
 		i++;
 	}
 	//sort by hours
-	qsort(&slg[0].hours, 20, sizeof(participants), comp);
+	qsort(&slg[0].hours, 6, sizeof(participants), comp);
 
 	printf("LEADERBOARD: ");
 	for (int j=0;j<i; j++){
 		printf("%d. %s   %d     %d sessions\n", j+1, slg[j].name, slg[j].hours, slg[j].sessions);
 	}
 
-	printf("Okay let's do the average time attended per session now");
+	printf("Okay let's do the average time attended per session now\n");
 
 	for (int j=0; j<i; j++){
-		slg[j].average = slg[j].hours/slg[j].sessions;
+		slg[j].average = (float)(slg[j].hours/slg[j].sessions);
 	}
 
 	//sort by averages
-	qsort(&slg[0].average, 20, sizeof(participants), comp);
+    qsort(slg, 20, sizeof(participants), comp);
 
-	printf("LEADERBOARD: ");
+	printf("LEADERBOARD: \n");
 	for (int j=0;j<i; j++){
-		printf("%d. %s   %d     %d sessions\n", j+1, slg[j].name, slg[j].average, slg[j].sessions);
+		printf("%d. %s   %.2lf     %d sessions\n", j+1, slg[j].name, slg[j].average, slg[j].sessions);
 	}
 
 	return 0;
@@ -40,9 +40,10 @@ int main(int argc, char const *argv[])
 
 //write the comp function
 int comp(const void*a, const void*b ){
-	int * avga, *avgb,
+	const participants * avga;
+	const participants *avgb;
 	avga = a;
 	avgb = b;
-	return a-b;
+	return ((int)avga->average)-((int)avgb->average);
 }
 
